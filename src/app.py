75fdf6323859f3d95d30013ea3245d21b888a0c3
Jeremy Scott <jeremyscott@Jeremys-MacBook-Pro.local>
2019-09-05 18:15:40 +1000

    Initial Load Data Work

--- src/app.py
+++ src/app.py
@@ -1,1 +1,22 @@
-print("Hello World")
+import numpy as np
+import pandas as pd
+from scipy import signal
+import matplotlib.pyplot as plt
+import math
+from sklearn import preprocessing
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import make_scorer, accuracy_score, confusion_matrix
+from sklearn.svm import SVC
+from sklearn.model_selection import GridSearchCV
+
+#a simple function for loading data and return dataframes for each set
+#i want to be able to seperate the data into different folders e.g. "raw" untouched
+#data, "clean" modified data etc.
+#The point is I want to be able to point to a folder and have the function load
+#the datasets within there as there own functions
+
+def LoadData(dataPath):
+    df = pd.read_csv('dataset/dataset_1.txt', sep=',', header=None)
+    return df
+
+if __name__ == '__main__':
